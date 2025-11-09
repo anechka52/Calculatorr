2b5ac4fa6a431d34e18cd110fc439c902565d17a
huawei <huawei@DESKTOP-VK62F6N>
2025-11-09 22:35:08 +0500

    Добавьте файлы проекта.

---
+++ ConsoleCalculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal static class ConsoleCalculator
+    {
+        public static double Count(int numOperation, double firstOperand, double secondOperand)
+        {
+            switch (numOperation)
+            {
+                case 1: return firstOperand + secondOperand;
+                case 2: return firstOperand - secondOperand;
+                case 3:
+                    return firstOperand * secondOperand;
+                case 4:
+                    if (secondOperand == 0) { return Double.PositiveInfinity; }
+                    else { return firstOperand / secondOperand; }
+                case 5: return Math.Pow(firstOperand, secondOperand);
+                default: return 0;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOperation;
+            double firstOperand, secondOperand, result = 0;
+            char userInput;
+            bool work = true;
+
+            while (work)
+            {
+                try
+                {
+                    Console.Write("Введите первое число: ");
+                    firstOperand = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("\nВведите второе число: ");
+                    secondOperand = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("\nВведите номер операции: (1:+ 2:- 3:* 4:/ 5:^): ");
+                    numOperation = Convert.ToInt32(Console.ReadLine());
+                    if (numOperation != 1 && numOperation != 2 && numOperation != 3 && numOperation != 4 && numOperation != 5)
+                    {
+                        Console.WriteLine("\nВведены некорректные данные!");
+                        Console.ReadKey();
+                        continue;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("\nВведены некорректные данные!");
+                    Console.ReadKey();
+                    continue;
+                }
+                if (secondOperand == 0 && numOperation == 4) Console.WriteLine($"\nРезультат: Бесконечность");
+                else if (double.IsInfinity(ConsoleCalculator.Count(numOperation, firstOperand, secondOperand))) Console.WriteLine($"\nРезультат: Слишком сложное число для простого калькулятора!");
+                else
+                    result = ConsoleCalculator.Count(numOperation, firstOperand, secondOperand);
+                Console.WriteLine($"\nРезультат: {result}");
+                while (true)
+                {
+                    try
+                    {
+                        Console.WriteLine("Желаете продолжить?(Y/N) (По умолчанию: N)");
+                        userInput = Convert.ToChar(Console.ReadLine());
+                        if (userInput == 'Y' || userInput == 'y') break;
+                        else
+                        {
+                            work = false;
+                            break;
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine("\nВведены некорректные данные!");
+                        Console.ReadKey();
+                        continue;
+                    }
+
+
+                }
+        }   }
+    }
+}
